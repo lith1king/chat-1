eb233b2a4e0368652a2a1fa42882d586ccb3c5e8
lith1king <46399211+lith1king@users.noreply.github.com>
2019-01-05 17:36:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content === '1say'){
-        message.channel.send(' @- FT, laith 5000')
+        message.channel.send(' @- FT, laith#7072 5000')
     }
 });
 
